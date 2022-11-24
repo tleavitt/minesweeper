902002e36c0a0dc2886fbfd3876b869e8e6ee2f5
tucker_leavitt <tucker.leavitt@gmail.com>
2022-11-24 13:02:52 -0300

    WIP

--- src/solve_grid.rs
+++ src/solve_grid.rs
@@ -5,11 +5,20 @@ use crate::grid::*;
 
 #[derive(Debug, Clone)]
 pub struct SolveCell {
-    mine_likelihood: f64, // The probability that this cell is a mine
+    mine_likelihood: f64, // The current estimated probability that this cell is a mine
 }
+
 
 // Internal board used by the solver
 type SolveGrid = Vec<Vec<SolveCell>>;
+
+pub fn flatten_cells(solve_grid: &SolveGrid) -> Vec<Vec<f64>> {
+    solve_grid.iter().map(
+        |row| row.iter().map(
+            |cell| cell.mine_likelihood
+        ).collect()
+    ).collect()
+}
 
 // Container for data structures the solver uses.
 pub struct SolveState {
@@ -41,7 +50,9 @@ pub fn init_solve_grid(nrows: usize, ncols: usize, nmines: usize) -> SolveGrid {
 
 ///
 /// Terms:
-/// settled cell: a cell that's either a known mine, or an marked cell where we know which of its neighbors are mines (if any)
+/// unknown cell: a cell with a non-zero and non-one mine probability
+/// settled cell: a cell that's either a known mine, or an marked cell where none of it's neighbors are unknown cells
+/// The goal is to convert all cells to settled cells.
 /// boundary cell: marked cell with a non-zero mine count that's not settled (i.e. we're not sure what all of it's mines are)
 /// frontier cell: unmarked, unsettled cell that borders a boundary cell (i.e. a candidate for being a mine)
 ///
@@ -59,9 +70,9 @@ pub fn mark(solve_state: &mut SolveState, mark_i: usize, mark_j: usize, mine_map
     for cur in &marked_cells {
         let (i, j) = cur;
         // Check if this is a boundary cell, and if so, add it to our output.
+        // Will be a boundary cell if it's got a nonzero mine count and it's not settled - i.e. it has some unsettled neighbors
         if get(&solve_state.count_grid, *i, *j).value > 0 {
-            // Will be a boundary cell if it's not settled - i.e. it has some unsettled neighbors
-            if !get_unsettled_neighbors(&solve_state.solve_grid, *i, *j).is_empty() {
+            if !get_unknown_neighbors(&solve_state.solve_grid, *i, *j).is_empty() {
                 new_boundary_cells.push(*cur);
             }
         }
@@ -69,14 +80,34 @@ pub fn mark(solve_state: &mut SolveState, mark_i: usize, mark_j: usize, mine_map
     new_boundary_cells
 }
 
-fn get_unsettled_neighbors(solve_grid: &SolveGrid, i: usize, j: usize) -> Vec<(usize, usize)>{
-    let mut unsettled_neighbors = Vec::with_capacity(8);
+/// Given a list of boundary cells, update the likelihoods for their neighboring frontier cells.
+///
+/// Invariant: the probabilities of a boundary cell's neighbors always sum to
+/// the boundary cell's count. i.e. if the count is 2, sum of all neighbors probabilities
+/// must equal 2.
+pub fn update_likelihoods(solve_state: &mut SolveState, new_boundary_cells: &Vec<(usize, usize)>) {
+    for (i_, j_) in new_boundary_cells {
+        let i = *i_; let j = *j_;
+        let mut known_neighbors = Vec::with_capacity(8);
+        let mut known_probability_mass = 0.0;
+        let mut unknown_neighbors = Vec::with_capacity(8);
+        let mut known_probability_mass = 0.0;
+        for_each_neighbor(&solve_state.solve_grid, i, j, &mut |cell, ni, nj| {
+            if cell.mine_likelihood == 0.0 && cell.mine_likelihood == 1.0 {
+                unknown_neighbors.push((i, j))
+            }
+        });
+    }
+}
+
+fn get_unknown_neighbors(solve_grid: &SolveGrid, i: usize, j: usize) -> Vec<(usize, usize)>{
+    let mut unknown_neighbors = Vec::with_capacity(8);
     for_each_neighbor(solve_grid, i, j, &mut |cell, ni, nj| {
         if cell.mine_likelihood != 0.0 && cell.mine_likelihood != 1.0 {
-            unsettled_neighbors.push((i, j))
+            unknown_neighbors.push((i, j))
         }
     });
-    unsettled_neighbors
+    unknown_neighbors
 }
 
 pub fn to_string(solve_grid: &SolveGrid) -> String {
@@ -113,9 +144,10 @@ mod tests {
         );
         println!("count_grid: {}", count_grid::to_string(&solve_state.count_grid));
         println!("solve_grid: {}", solve_grid::to_string(&solve_state.solve_grid));
-        let marked_cells = mark(&mut solve_state, 0, 2, &mine_map);
+        let new_boundary_cells = mark(&mut solve_state, 0, 2, &mine_map);
         println!("{}", count_grid::to_string(&solve_state.count_grid));
         println!("{}", solve_grid::to_string(&solve_state.solve_grid));
+        println!("{:?}", new_boundary_cells);
 
         assert_eq!(
             vec![
@@ -126,7 +158,7 @@ mod tests {
                 vec![ 0,  0,  0,  1,  1],
                 vec![ 0,  0,  0,  1, -1],
             ],
-            flatten_cells(&solve_state.count_grid)
+            flatten_cells(&solve_state.solve_grid)
         )
     }
 }
